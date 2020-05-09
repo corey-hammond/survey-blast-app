196f2d6f76321c7373655264e59fcf749d019d13
Corey Hammond <coreyjhammond@gmail.com>
2020-05-09 14:40:26 -0700

    Revert "developed production set for db, google api, and cookie key"
    This reverts commit 383249bfd6889d1e717aea3541370adc8ca0ccbd.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
-// Use cookie-session middleware
+// Use cookie-session
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
