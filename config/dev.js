7ba67d6a2557eb798c194cddf939f1b9342660ec
Corey Hammond <coreyjhammond@gmail.com>
2020-05-09 14:42:36 -0700

    developed production set for db, google api, and cookie key

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this
+module.exports = {
+  googleClientID:
+    "1089662194295-4v04k0v54pje5okk2v7fua7c0dm9vtok.apps.googleusercontent.com",
+  googleClientSecret: "H0qxiymTNAZ8zejSmn6ayRS_",
+  mongoURI:
+    "mongodb+srv://admin:twJkUaHM2vlp29ej@main-un1as.mongodb.net/SurveyBlasterDB?retryWrites=true&w=majority",
+  cookieKey: "adpcezccde",
+};
