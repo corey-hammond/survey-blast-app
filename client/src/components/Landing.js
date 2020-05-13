4eb6b589a5758df1bdf4b48d898cd433e5f7e6e8
Corey Hammond <coreyjhammond@gmail.com>
2020-05-13 15:57:15 -0700

    created Landing component; configured Link to dashboard or landing components

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="center">
+      <h1>Survey Blaster</h1>
+      <p>Collect feedback from your users.</p>
+    </div>
+  );
+};
+
+export default Landing;
