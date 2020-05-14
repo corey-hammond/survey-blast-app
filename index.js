da19eb8cf66b3d14945f2d63e50f90ac067ce63c
Corey Hammond <coreyjhammond@gmail.com>
2020-05-13 20:58:07 -0700

    installed and connected body-parser middleware to express server

--- index.js
+++ index.js
@@ -3,27 +3,38 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 require("./models/User");
 require("./services/passport");
 
+// Connect to database
 mongoose.connect(keys.mongoURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 
+// Instantiate Express app
 const app = express();
+
+// Use body-parser
+app.use(bodyParser.json());
 
 // Use cookie-session
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    maxAge: 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
+
+// Use passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Routes
 require("./routes/auth")(app);
+require("./routes/billing")(app);
 
+// Server connection
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
