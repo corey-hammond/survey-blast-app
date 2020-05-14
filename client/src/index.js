852b5b0733ff575277d9227e1d620e8a2731a8eb
Corey Hammond <coreyjhammond@gmail.com>
2020-05-13 19:15:29 -0700

    added Stripe keys to project

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxThunk from 'redux-thunk'
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
-import reducers from './reducers'
+import reducers from "./reducers";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,6 +14,6 @@ ReactDom.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector("#root") 
+  document.querySelector("#root")
 );
- 
+
