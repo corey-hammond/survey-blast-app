e83ec680b83988b1653b95913566a9b18f1d6155
Corey Hammond <coreyjhammond@gmail.com>
2020-05-13 19:26:53 -0700

    created StripeWrapper component and imported into Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import StripeWrapper from './StripeWrapper'
 
 class Header extends Component {
   renderContent() {
